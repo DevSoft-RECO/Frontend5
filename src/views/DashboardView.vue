c59533d3b68557df66aba71f04352ef4d2c7af6a
DevSoft-RECO <ronycardonaordonez@gmail.com>
2026-03-02 11:09:32 -0600

    mejoras de modo pantalla copelta

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -82,7 +82,7 @@
         <svg class="w-64 h-64" fill="currentColor" viewBox="0 0 24 24"><path d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>
       </div>
 
-      <div class="relative z-10">
+      <div class="relative z-10" :class="[isFullscreen ? 'flex-1 flex flex-col' : '']">
         <div class="flex flex-col xl:flex-row xl:items-center justify-between gap-6 mb-8">
             <div class="space-y-4">
                 <div>
@@ -294,6 +294,8 @@ onMounted(() => {
     border: none;
     background: transparent;
     box-shadow: none;
+    display: flex;
+    flex-direction: column;
 }
 
 /* Force dark mode background in fullscreen if needed */
